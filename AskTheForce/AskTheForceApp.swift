8a77c354ead34e832532443a9f8971fe169cb560
Cas9Diego <DaCastro@outlook.com>
2022-08-20 17:20:19 -0500

    Initial Commit

---
+++ AskTheForce/AskTheForceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskTheForceApp.swift
+//  AskTheForce
+//
+//  Created by Diego Castro on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct AskTheForceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
